ae373d552ffcf970ebc4d093fcb622a06364f8ff
Pranjeet Dhanapune <pranjeetdhanapune@gmail.com>
2021-05-20 00:24:06 +0530

    firebase functions

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // THE API (cloud function) URL
-  baseURL: "http://localhost:5001/clone-d1894/us-central1/api",
+  baseURL: "http://localhost:5001/clone-6eb4b/us-central1/api",
 });
 
 export default instance;
